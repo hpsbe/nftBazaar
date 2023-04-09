98f94940024b3c9cafc2b810b1c4a1829ed781de
Hargunpartap Singh Badwal <hpsb@duck.com>
2023-04-09 16:42:41 +0530

    Update NFTMarketplaceContext.js

--- Context/NFTMarketplaceContext.js
+++ Context/NFTMarketplaceContext.js
@@ -13,7 +13,7 @@ const auth = `Basic ${Buffer.from(`${projectId}:${projectSecretKey}`).toString(
 
 const subdomain = process.env.NEXT_PUBLIC_SUBDOMAIN;
 
-const client = create({
+const client = ipfsHttpClient({
   host: "infura-ipfs.io",
   port: 5001,
   protocol: "https",
